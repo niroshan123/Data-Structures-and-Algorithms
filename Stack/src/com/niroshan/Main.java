4a010804c24c8ae66484bcb34bef761b9a099e99
niroshan123 <niroshanratnayake07@gmail.com>
2021-07-06 14:24:09 +0530

    Test

--- Stack/src/com/niroshan/Main.java
+++ Stack/src/com/niroshan/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello Niroshan");
+        System.out.println("Hello Niroshan Ratnayake");
     }
 }
